ba07f99ea8329a7d2593c95ad9f884daaa2d8799
Achilles_Star <paulwilstar1225@gmail.com>
2024-03-08 21:16:33 -0500

    update

---
+++ React/website/my-react-app1/src/App.jsx
@@ -0,0 +1,20 @@
+// props = read-only properties that are shared between components.
+//         A parent component can send data to a child component.
+//         <Component key=value />
+
+import Student from './Student.jsx'
+
+function App() {
+
+    return(
+      <>
+        <Student name="Spongebob" age={30} isStudent={true}/>
+        <Student name="Patrick" age={42} isStudent={false}/>
+        <Student name="Squidward" age={50} isStudent={false}/>
+        <Student name="Sandy" age={27} isStudent={true}/>
+        <Student name="Larry"/>
+      </>    
+    );
+}
+
+export default App
